32e2898bfd1d634dcdeefe0366aa32eb894ea0c9
LUCAS MONTEIRO PINTO DE AMORIM <lucasZero@users.noreply.github.com>
2022-05-03 16:20:38 -0300

    Implementado classes de modelo

---
+++ src/main/java/com/lucas/santander/banklineapi/model/Conta.java
@@ -0,0 +1,25 @@
+package com.lucas.santander.banklineapi.model;
+
+public class Conta {
+    
+    private Long numero;
+    private Double saldo;
+
+    public Long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Long numero) {
+        this.numero = numero;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+    
+    
+}
---
+++ src/main/java/com/lucas/santander/banklineapi/model/MovimentacaoTipo.java
@@ -0,0 +1,5 @@
+package com.lucas.santander.banklineapi.model;
+
+public enum MovimentacaoTipo {
+    RECEITA, DESPESA
+}
