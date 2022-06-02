6e5f38337075566bd1a4a496fa570f85bccb03f9
LUCAS MONTEIRO PINTO DE AMORIM <lucasZero@users.noreply.github.com>
2022-06-02 15:02:40 -0300

    Implementado a consulta de movimentação filtrado por conta

--- src/main/java/com/lucas/santander/banklineapi/controller/MovimentacaoController.java
+++ src/main/java/com/lucas/santander/banklineapi/controller/MovimentacaoController.java
@@ -7,9 +7,11 @@ import com.lucas.santander.banklineapi.service.MovimentacaoService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,6 +26,11 @@ public class MovimentacaoController {
     @GetMapping
     public List<Movimentacao> findAll(){
         return repository.findAll();
+    }
+    
+    @GetMapping("/{idConta}")
+    public List<Movimentacao> findAll(@PathVariable("idConta") int idConta){
+        return repository.findByIdConta(idConta);
     }
     
     @PostMapping
--- src/main/java/com/lucas/santander/banklineapi/repository/MovimentacaoRepository.java
+++ src/main/java/com/lucas/santander/banklineapi/repository/MovimentacaoRepository.java
@@ -1,8 +1,11 @@
 package com.lucas.santander.banklineapi.repository;
 
 import com.lucas.santander.banklineapi.model.Movimentacao;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Integer>{
+    
+    public List<Movimentacao>findByIdConta(int idConta);
     
 }
